8d5c0900a9839734b54b1d4494692e89fc9f4ad4
hwlvd <lvhongwei02@gmail.com>
2023-01-11 11:44:25 +0800

    comp

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import LuckySheet from "./components/LuckySheet.vue";
 import { Col, List, Row, Tree } from "ant-design-vue";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 const currentCount = ref(0);
 const list = ref([
   {
@@ -49,7 +49,7 @@ const hanleList = (count) => {
   isdrag.value = true;
   currentCount.value = count;
 };
-const treeData = ref([
+const treeData = [
   {
     title: "parent 1",
     key: "0-0",
@@ -63,11 +63,8 @@ const treeData = ref([
             key: "0-0-0-0",
           },
           {
-            key: "0-0-0-1",
-          },
-          {
             title: "leaf",
-            key: "0-0-0-2",
+            key: "0-0-0-1",
           },
         ],
       },
@@ -76,22 +73,8 @@ const treeData = ref([
         key: "0-0-1",
         children: [
           {
-            title: "leaf",
             key: "0-0-1-0",
-          },
-        ],
-      },
-      {
-        title: "parent 1-2",
-        key: "0-0-2",
-        children: [
-          {
-            title: "leaf 1",
-            key: "0-0-2-0",
-          },
-          {
-            title: "leaf 2",
-            key: "0-0-2-1",
+            title: "sss",
           },
         ],
       },
@@ -99,25 +82,19 @@ const treeData = ref([
   },
   {
     title: "parent 2",
-    key: "0-1",
+    key: "1-0",
     children: [
       {
         title: "parent 2-0",
-        key: "0-1-0",
-        children: [
-          {
-            title: "leaf",
-            key: "0-1-0-0",
-          },
-          {
-            title: "leaf",
-            key: "0-1-0-1",
-          },
-        ],
+        key: "1-0-0",
+      },
+      {
+        title: "parent 2-1",
+        key: "2-0-1",
       },
     ],
   },
-]);
+];
 const overDrag = () => {
   if (isdrag.value === true) {
     console.log("-拖拽中");
@@ -129,6 +106,18 @@ const mouseUp = () => {
   // console.log("松开了");
   isdrag.value = false;
 };
+const selectedKeys = ref(["0-0-0", "0-0-1"]);
+const checkedKeys = ref(["0-0-0", "0-0-1"]);
+const expandedKeys = ref([]);
+watch(expandedKeys, () => {
+  console.log("expandedKeys", expandedKeys);
+});
+watch(selectedKeys, () => {
+  console.log("selectedKeys", selectedKeys);
+});
+watch(checkedKeys, () => {
+  console.log("checkedKeys", checkedKeys);
+});
 const mouseOver = () => {
   if (isdrag.value === true) {
     console.log("出来了哦");
@@ -136,6 +125,17 @@ const mouseOver = () => {
 };
 const cancelDrag = () => {
   isdrag.value = false;
+};
+const handleExpand = (keys, { expanded, node }) => {
+  // node.parent add from 3.0.0-alpha.10
+  const tempKeys = ((node.parent ? node.parent.children : treeData) || []).map(
+    ({ key }) => key
+  );
+  if (expanded) {
+    expandedKeys.value = _.difference(keys, tempKeys).concat(node.key);
+  } else {
+    expandedKeys.value = keys;
+  }
 };
 </script>
 <template>
@@ -154,23 +154,33 @@ const cancelDrag = () => {
           {{ item.count }}
         </div>
       </List.Item> -->
-      <Tree>
-        <List.Item
-          @mouseover="mouseOver"
-          @mouseleave="overDrag"
-          v-for="(item, index) in list"
-          :key="item.id"
-          ><div
-            style="cursor: pointer"
-            @mousedown="hanleList(item.count)"
-            @mouseup="mouseUp"
-          >
-            {{ item.count }}
-          </div>
-        </List.Item>
+      <Tree
+        v-model:selectedKeys="selectedKeys"
+        :expanded-keys="expandedKeys"
+        :tree-data="treeData"
+        @expand="handleExpand"
+      >
+        <template #title="{ title, key }">
+          <!-- <span v-if="key === '0-0-1-0'" style="color: #1890ff">{{ title }}</span> -->
+          <List.Item
+            v-if="key === '0-0-1-0'"
+            @mouseover="mouseOver"
+            @mouseleave="overDrag"
+            v-for="(item, index) in list"
+            :key="item.id"
+            ><div
+              style="cursor: pointer"
+              @mousedown="hanleList(item.count)"
+              @mouseup="mouseUp"
+            >
+              {{ item.count }}
+            </div>
+          </List.Item>
+          <template v-else>{{ title }}</template>
+        </template>
       </Tree>
     </Col>
-    <Col style="width: 96%">
+    <Col style="width: 90%; height: 800px">
       <LuckySheet
         :isdrag="isdrag"
         :count="currentCount"
